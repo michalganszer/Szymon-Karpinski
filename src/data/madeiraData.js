d802867929f3a794a7781dbad4ee0952f64ba95e
michalganszer <michal.ganszer@gmail.com>
2020-08-10 11:38:38 +0200

    Update madeiraData.js

--- src/data/madeiraData.js
+++ src/data/madeiraData.js
@@ -1,7 +1,11 @@
+import a from '../assets/images/Series/Madeira/1.jpg';
+import b from '../assets/images/Series/Madeira/2.jpg';
+
 export const slides = [
-    { image: "https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg", info: "description 1" },
-    { image: "https://miro.medium.com/max/3000/1*MI686k5sDQrISBM6L8pf5A.jpeg", info: "description 2" },
-    { image: "https://live.staticflickr.com/4561/38054606355_26429c884f_b.jpg", info: "description 3" }
-  ];
+  { image: a, info: "description 1" },
+  { image: b, info: "description 2" },
+  { image: a, info: "description 1" },
+  { image: b, info: "description 2" },  
+];
 
   
