013041721c1d272c0c3cff0ab4776beb5e6acbec
michalganszer <michal.ganszer@gmail.com>
2020-08-10 11:38:48 +0200

    Update Kontakt.js

--- src/views/Kontakt/Kontakt.js
+++ src/views/Kontakt/Kontakt.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import styles from './Kontakt.module.scss';
+import { LanguageContext } from '../../contexts/LanguageContext';
 
 const Kontakt = () =>(
-<div className={styles.kontakt}>
 
-    <div>karpinski_szymon@wp.pl</div>
-    <a href="tel:+48514942473"> (+48) 514-942-473</a>
-    
+    <LanguageContext.Consumer>{(context)=>{
+        return(
+            <>
+            {context.isPL ? <PL/> : <EN/>}
+            </>        
+        )
+    }}
 
-    <p>Szymon Karpiński ©  2020</p>
-</div>
+    </LanguageContext.Consumer>
+
 );
+
+const PL = () =>(
+    <div className={styles.kontakt}>
+               
+    <div>karpinski_szymon@wp.pl</div>
+        <a href="tel:+48514942473"> (+48) 514-942-473</a>
+    <p>Szymon Karpiński ©  2020</p>
+ </div>
+)
+
+const EN = () =>(
+    <div className={styles.kontakt}>
+               
+    <p>Go back to PL</p>
+ </div>
+)
 
 export default Kontakt;
