a5efc396951833ef6145d6d9735d051ff246dc18
michalganszer <michal.ganszer@gmail.com>
2020-08-10 11:38:04 +0200

    Update Menu.js

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,22 +3,33 @@ import{ NavLink } from 'react-router-dom';
 import styles from './Menu.module.scss';
 import facebook from '../../assets/images/social/facebook.png';
 import instagram from '../../assets/images/social/instagram.png';
+import { LanguageContext } from '../../contexts/LanguageContext';
 
 const Menu = ({openClose}) =>(
-<nav>
-    <ul className={styles.navItems}>
-        <li onClick={openClose} className={styles.navItem}><NavLink className={styles.navItemLink} activeClassName={styles.navItemLinkActive} to="/madeira">Madeira</NavLink></li>
-        <li onClick={openClose} className={styles.navItem}><NavLink className={styles.navItemLink} activeClassName={styles.navItemLinkActive} to="/Nieuchronne-straty-i-wartościowe-zmiany">Nieuchronne straty i wartościowe zmiany</NavLink></li>
-        <li onClick={openClose} className={styles.navItem}><NavLink className={styles.navItemLink} activeClassName={styles.navItemLinkActive} to="/teksty">Teksty</NavLink></li>
-        <li onClick={openClose} className={styles.navItem}><NavLink className={styles.navItemLink} activeClassName={styles.navItemLinkActive} to="/bio">Bio</NavLink></li>
-        <li onClick={openClose} className={styles.navItem}><NavLink className={styles.navItemLink} activeClassName={styles.navItemLinkActive} to="/kontakt">Kontakt</NavLink></li>
-    </ul>
-    <div className={styles.socialIcons}>
-    <img src={facebook}></img>
-    <img src={instagram}></img>
-    </div>
-    
-</nav>
+<LanguageContext.Consumer>{(context)=>{
+    const {setPL, setEN} = context;
+        return(
+            <nav>
+                <ul className={styles.navItems}>
+                    <li onClick={openClose} className={styles.navItem}><NavLink className={styles.navItemLink} activeClassName={styles.navItemLinkActive} to="/madeira">Madeira</NavLink></li>
+                    <li onClick={openClose} className={styles.navItem}><NavLink className={styles.navItemLink} activeClassName={styles.navItemLinkActive} to="/Nieuchronne-straty-i-wartościowe-zmiany">Nieuchronne straty i wartościowe zmiany</NavLink></li>
+                    <li onClick={openClose} className={styles.navItem}><NavLink className={styles.navItemLink} activeClassName={styles.navItemLinkActive} to="/teksty">Teksty</NavLink></li>
+                    <li onClick={openClose} className={styles.navItem}><NavLink className={styles.navItemLink} activeClassName={styles.navItemLinkActive} to="/bio">Bio</NavLink></li>
+                    <li onClick={openClose} className={styles.navItem}><NavLink className={styles.navItemLink} activeClassName={styles.navItemLinkActive} to="/kontakt">Kontakt</NavLink></li>
+                </ul>
+                <div className={styles.socialIcons}>
+                    <img src={instagram}></img>
+                    <img src={facebook}></img>
+                </div>
+                
+                <div className={styles.language}>
+                    <button onClick={setPL}>pl</button><span>|</span><button onClick={setEN}>en</button>
+                </div>
+            </nav>
+
+            )
+        }}
+    </LanguageContext.Consumer>
 );
 
 export default Menu;
