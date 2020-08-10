9c06193f967b5d27f60fbc5db88c0afc33b2073c
michalganszer <michal.ganszer@gmail.com>
2020-08-10 11:37:53 +0200

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
 import styles from './Header.module.scss'
 import Hamburger from './Hamburger/Hamburger';
 import H1 from './H1/H1';
 
 const Header = ({openClose, modalState}) =>(
 <header className={styles.header}>
-    <H1/>
-    <Hamburger modalState={modalState} openClose={openClose} />
+    
+     <H1/>
+    
+    <MediaQuery maxWidth={899}>
+        <Hamburger modalState={modalState} openClose={openClose} />
+    </MediaQuery>
 </header>
     
 );
