c818727c571b95f5b17aa9ea05b2122440d09269
michalganszer <michal.ganszer@gmail.com>
2020-08-10 11:37:07 +0200

    Update GalleryClick.js

--- src/components/Galleries/GalleryClick/GalleryClick.js
+++ src/components/Galleries/GalleryClick/GalleryClick.js
@@ -1,100 +1,107 @@
 import React from 'react';
-import styles from './GalleryClick.module.scss';
+import AliceCarousel from 'react-alice-carousel';
 import prev from './../../../assets/images/icons/prev.png';
 import next from './../../../assets/images/icons/next.png';
-import MediaQuery from 'react-responsive';
-import Slide from '../Slide/Slide';
+import './GalleryClick.scss';
+import Info from '../Info/Info'
 
-class GalleryClick extends React.Component{
-    state = {
-        currentIndex: 0,
-        slides: this.props.slides,
-        info:this.props.info,
-        showInfo: false,
-        showInfoX: false
+
+
+class Slider extends React.Component {
+  state = {
+    slides: this.props.slides,
+    showInfo:false,
+    showInfoAnimate:3,
+    currentIndex: 0,
+  }
+
+  slideTo = (i) => this.setState({ currentIndex: i })
+
+  onSlideChanged = (e) => this.setState({ currentIndex: e.item })
+
+  slideNext = () => this.setState({ currentIndex: this.state.currentIndex + 1 })
+
+  slidePrev = () => this.setState({ currentIndex: this.state.currentIndex - 1 })
+
+  thumbItem = (item, i) => <span onClick={() => this.slideTo(i)}>* </span>
+
+  
+  
+  animateInfo = () =>{
+    //this.setState(prevState => ({
+    //  showInfoAnimate: !prevState.showInfoAnimate}))
+    if (this.state.showInfoAnimate === 3){
+      this.setState({
+        showInfoAnimate: 1
+      })
+      setTimeout(()=>{this.setState({
+        showInfoAnimate: 2
+      })},700)
     }
-
-    goToPrevSlide = () => {
-        if(this.state.currentIndex != 0)        
-        this.setState(prevState => ({
-          currentIndex: prevState.currentIndex - 1
-        }))
-      }
-
-    goToNextSlide = () => {
-        if(this.state.currentIndex != (this.state.slides.length - 1))
-        this.setState(prevState => ({
-            currentIndex: prevState.currentIndex + 1
-          }))
+    else {
+      this.setState({
+        showInfoAnimate: 3
+      })
     }
+  }
 
-    showInfoHandler = () =>{
-        this.setState(prevState => ({
-            showInfoX: !prevState.showInfoX}))
-
-        this.state.showInfo === true ?
+  showInfoHandler = () =>{
         
-        (setTimeout(() =>{
+    this.animateInfo()
+
+    this.state.showInfo  ?
+    
+    (setTimeout(() =>{
+    this.setState(prevState => ({
+        showInfo: !prevState.showInfo
+      }))}, 700)
+      
+
+      ) : (
+
         this.setState(prevState => ({
-            showInfo: !prevState.showInfo
-          }))}, 700)
-          
-          ) : (
-
-            this.setState(prevState => ({
-            showInfo: !prevState.showInfo})))
+        showInfo: !prevState.showInfo})));
+        setTimeout(() =>{
+        window.scrollTo({top:document.body.scrollHeight,behavior: 'smooth' });}, 100)        
     }
 
 
-    render(){
-        return(
-                <div className={styles.wrapper}>
-                    <div className={styles.slideWrapper}>
-                        {this.state.slides.map((slide, i) =>(
+  render() {
+    const {currentIndex } = this.state
+    return (
+      <div>
+      <div className="alice-carousel-custom-wrapper">
+        <AliceCarousel
+          dotsDisabled={true}
+          buttonsDisabled={true}
+          slideToIndex={currentIndex}
+          onSlideChanged={this.onSlideChanged}
+        >
 
-                        (i === this.state.currentIndex) 
-                        && 
-                        <Slide key={i} image={slide.image} />  
+        {this.state.slides.map((slide, i) =>(
 
-                        ))}
-                         <MediaQuery maxWidth={899}> 
-                            {(this.state.currentIndex != (this.state.slides.length - 1)) && <Next goToNextSlide={this.goToNextSlide}/>}
-                            {(this.state.currentIndex != 0) && <Prev goToPrevSlide={this.goToPrevSlide}/>}
-                        </MediaQuery> 
-                    </div>
-                    <div className={styles.slideNav}>
-                        <div className={styles.buttons}>
-                            
-                            <MediaQuery minWidth={900}> 
-                                {(this.state.currentIndex != 0) ? <Prev goToPrevSlide={this.goToPrevSlide}/> : <span>&nbsp;</span>}
-                                {(this.state.currentIndex != (this.state.slides.length - 1)) && <Next goToNextSlide={this.goToNextSlide}/> }
-                            </MediaQuery> 
-                            
-                            &nbsp;</div>
-                        <div className={styles.imageCounter}>{this.state.currentIndex+1}/{this.state.slides.length}</div>
-                        <button onClick={this.showInfoHandler} className={styles.info}>info</button>
-                    </div>
-                    {this.state.slides.map((slide, i) =>(
-                    this.state.showInfo && (i === this.state.currentIndex) && <Info key={i} showInfo={this.state.showInfoX} info={slide.info}/>))}
-                    
-                  
-                </div>
-            )
-        }
-    };
+        <img key={i} src={slide.image} className="alice-carousel__slide" />  
+
+        ))}
+        </AliceCarousel>
 
 
-const Info = ({ info, showInfo }) =>(
-    <p className={showInfo ? styles.infoContentFadeIn : styles.infoContentFadeOut}>{info}</p>
-)
+        <button className="alice-carousel__prev-btn-custom" onClick={() => this.slidePrev()}><img src={prev}/></button>
+        <button className="alice-carousel__next-btn-custom" onClick={() => this.slideNext()}><img src={next}/></button>
+      
 
-const Prev = ({goToPrevSlide}) =>{
-    return <div className={styles.buttonPrev} onClick={goToPrevSlide} ><img src={prev}/></div>
-    }
+        
+      </div>
+      <div className="alice-carousel__buttonWrapper">
+            <button onClick={this.showInfoHandler} className="alice-carousel__info">INFO</button>
+        </div>
 
-const Next = ({goToNextSlide}) =>{
-      return <div className={styles.buttonNext} onClick={goToNextSlide} ><img src={next}/></div>
-    }
+        {this.state.slides.map((slide, i) =>(
+          this.state.showInfo && (i === this.state.currentIndex) && <Info key={i}  animate={this.state.showInfoAnimate} info={slide.info}/>))}
+      </div>
+    )
+  }
+}
 
+export default Slider
 
-export default GalleryClick;
