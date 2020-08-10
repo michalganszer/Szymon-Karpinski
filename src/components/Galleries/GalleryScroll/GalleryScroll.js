f044ebbab9e0520ba3187660b5541fcbf5534a69
michalganszer <michal.ganszer@gmail.com>
2020-08-10 11:37:22 +0200

    Update GalleryScroll.js

--- src/components/Galleries/GalleryScroll/GalleryScroll.js
+++ src/components/Galleries/GalleryScroll/GalleryScroll.js
@@ -6,28 +6,51 @@ import Slide from '../Slide/Slide';
 class GalleryScroll extends React.Component{
     state = {
         slides: this.props.slides,
-        showInfo: false
+        showInfo: false,
+        showInfoAnimate:false
     }
 
-    showInfoHandler = () =>{
+    animateInfo = () =>{
+        this.setState(prevState => ({
+          showInfoAnimate: !prevState.showInfoAnimate}))
+        
+      }
+    
+      showInfoHandler = () =>{
+            
+        this.animateInfo()
+        
+    
+        this.state.showInfo  ?
+        
+        (setTimeout(() =>{
         this.setState(prevState => ({
             showInfo: !prevState.showInfo
-          }));
-    }
+          }))}, 700)
+          
+    
+          ) : (
+    
+            this.setState(prevState => ({
+            showInfo: !prevState.showInfo})));
+            setTimeout(() =>{
+            window.scrollTo({top:document.body.scrollHeight,behavior: 'smooth' });}, 100)        
+        }
+
 render(){
 return(
 <div className={styles.wrapper}>
-<div className={styles.galleryScroll}>
-    {this.state.slides.map((slide, i) =>(
+    <div className={styles.galleryScroll}>
+        {this.state.slides.map((slide, i) =>(
 
-        <Slide secondary key={i} image={slide.image} /> 
-        
-        ))}
+            <Slide key={i} image={slide.image} /> 
+            
+            ))}
         
     </div>
-
-    <button onClick={this.showInfoHandler} className={styles.info}>info</button>
-    {this.state.showInfo && <p className={styles.infoContent}>{this.state.slides[0].info}</p>}
+    
+    <button onClick={this.showInfoHandler} className={styles.info}>INFO</button>
+    {this.state.showInfo && <p className={this.state.showInfoAnimate ? styles.infoContentFadeIn : styles.infoContentFadeOut}>{this.state.slides[0].info}</p>}
 </div>
     
  ) }}
