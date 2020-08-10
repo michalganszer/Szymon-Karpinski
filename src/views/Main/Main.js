16aeaf2223ec78454bd903f6e44080962ac327c9
michalganszer <michal.ganszer@gmail.com>
2020-08-10 11:38:52 +0200

    Update Main.js

--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -2,11 +2,12 @@ import React from 'react';
 import main from '../../assets/images/main.jpg'
 import styles from './Main.module.scss';
 
+
 const Main = () =>(
-    <>
-<img className={styles.mainImage} src={main}/>
-<p className={styles.by}>by Michał Ganszer</p>
-</>
+    <div className={styles.main}>
+        <img className={styles.mainImage} src={main}/>
+        <p className={styles.by}>by Michał Ganszer</p>
+    </div>
 );
 
 export default Main;
