de25dbe629e8cfbf3fc97d74dad94ce6ff0fc765
michalganszer <michal.ganszer@gmail.com>
2020-08-10 11:36:06 +0200

    Update TekstWpis.js

--- src/components/TekstWpis/TekstWpis.js
+++ src/components/TekstWpis/TekstWpis.js
@@ -5,9 +5,10 @@ import linkImg from '../../assets/images/icons/link.png';
 
 const TekstWpis = ({children, link, pdf}) =>(
     <div className={styles.tekstWpisWrapper}>
-        <p className={styles.children}>{children}</p>
+        <p className={styles.children}>{children}
         {pdf!="" && <a href={pdf}><img className={styles.img} alt='pdf icon' src={pdfImg}></img></a>}
         {link!="" && <a href={link}><img className={styles.img} alt='link icon' src={linkImg}></img></a>}
+        </p>
     </div>
 );
 
